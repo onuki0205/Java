00650c1ed44b7971b467d7816a52533cb5ff8388
Shunsuke Onuki <41815571+onuki0205@users.noreply.github.com>
2025-05-14 23:30:48 +0900

    fix

---
+++ Traning/Puddle.java
@@ -0,0 +1,54 @@
+
+import java.util.*;
+
+public class Puddle {
+
+    private static int n;
+    private static int m;
+    private static char[][] puddle;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        puddle = new char[n][m];
+
+        // Read the puddle matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                puddle[i][j] = sc.next().charAt(0);
+            }
+        }
+
+        // dfs to count the number of puddles
+        int puddleCount = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (puddle[i][j] == "#") {
+                    dfs(i, j);
+                    puddleCount++;           
+                }
+            }  
+        }
+
+        // Print the number of puddles
+        System.out.println(puddleCount);
+        sc.close();
+
+    }
+
+    public static void dfs(int x, int y){
+        puddle[x][y] = ".";
+        for(int i=-1; i<=1; i++){
+            for(int j=-1; j<=1; j++){
+                if(i==0 && j==0) continue;
+                int newX = x + i;
+                int newY = y + j;
+                if(newX >= 0 && newX < n && newY >= 0 && newY < m && puddle[newX][newY] == "#"){
+                    dfs(newX, newY);
+                }
+            }
+        }
+        return;
+    }
+}
